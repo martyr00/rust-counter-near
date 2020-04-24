7193a0c53b9e2cf5890fbbc083b21a4b2909620e
Mike Purvis <mikedotexe@gmail.com>
2020-04-24 08:09:51 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+Counter example in Rust
+=================================
+
+[![Open in Gitpod!](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/near-examples/rust-counter)
+
+<!-- MAGIC COMMENT: DO NOT DELETE! Everything above this line is hidden on NEAR Examples page -->
+
+## Description
+
+This contract implements simple counter backed by storage on blockchain.
+Contract in `contract/src/lib.rs` provides methods to increment / decrement counter and get it's current value or reset.
+
+Plus and minus buttons increase and decrease value correspondingly. When button L is toggled, counter will add or minus 10 a time. RS button is for reset. LE and RE buttons to let the robot wink at you.
+
+## To Run
+Open in the Gitpod link above or clone the repository.
+
+```
+git clone https://github.com/near-examples/rust-counter
+```
+
+
+## Setup [Or skip to Login if in Gitpod](#login)
+Install dependencies:
+
+```
+yarn
+```
+
+Make sure you have `near-shell` by running:
+
+```
+near --version
+```
+
+If you need to install `near-shell`:
+
+```
+npm install near-shell -g
+```
+
+## Login
+If you do not have a NEAR account, please create one with [NEAR Wallet](https://wallet.nearprotocol.com).
+
+In the project root, login with `near-shell` by following the instructions after this command:
+
+```
+near login
+```
+
+Modify the top of `src/config.js`, changing the `CONTRACT_NAME` to be the NEAR account that was just used to log in.
+
+```javascript
+…
+const CONTRACT_NAME = 'YOUR_ACCOUNT_NAME_HERE'; /* TODO: fill this in! */
+…
+```
+
+Start the example!
+
+```
+yarn start
+```
+
+## To Test
+
+```
+cd contract
+cargo test -- --nocapture
+```
+
+## To Explore
+
+- `contract/src/lib.rs` for the contract code
+- `src/index.html` for the front-end HTML
+- `src/main.js` for the JavaScript front-end code and how to integrate contracts
+- `src/test.js` for the JS tests for the contract
---
+++ contract/src/lib.rs
@@ -0,0 +1,133 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen};
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// add the following attributes to prepare your code for serialization and invocation on the blockchain
+// More built-in Rust attributes here: https://doc.rust-lang.org/reference/attributes.html#built-in-attributes-index
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Counter {
+    val: i8, // i8 is signed. unsigned integers are also available: u8, u16, u32, u64, u128
+}
+
+#[near_bindgen]
+impl Counter {
+    // returns 8-bit signed integer, must match the type from our struct's 'val' defined above
+    // note the parameter is &self (without being mutable) meaning it doesn't modify state
+    pub fn get_num(&self) -> i8 {
+        return self.val;
+    }
+
+    // increment the counter
+    // note the parameter is "&mut self" as this function modifies state
+    pub fn increment(&mut self) {
+        // note: adding one like this is an easy way to accidentally overflow
+        // real smart contracts will want to have safety checks
+        self.val = self.val + 1;
+        let log_message = format!("Increased number to {}", self.val);
+        env::log(log_message.as_bytes());
+        after_counter_change();
+    }
+
+    // decrement (subtract from) the counter
+    pub fn decrement(&mut self) {
+        // note: subtracting one like this is an easy way to accidentally overflow
+        // real smart contracts will want to have safety checks
+        self.val = self.val - 1;
+        let log_message = format!("Decreased number to {}", self.val);
+        env::log(log_message.as_bytes());
+        after_counter_change();
+    }
+
+    // reset to zero
+    pub fn reset(&mut self) {
+        self.val = 0;
+        // Another way to log is to cast a string into bytes, hence "b" below:
+        env::log(b"Reset counter to zero");
+    }
+}
+
+// unlike the struct's functions above, this function cannot use attributes #[derive(…)] or #[near_bindgen]
+// any attempts will throw helpful warnings upon 'cargo build'
+// while this function cannot be invoked directly on the blockchain, it can be called from an invoked function
+pub fn after_counter_change() {
+    // show helpful warning that i8 (8-bit signed integer) will overflow above 127 or below -128
+    env::log("Make sure you don't overflow, my friend.".as_bytes());
+}
+
+
+/*
+ * the rest of this file sets up unit tests
+ * to run these, the command will be:
+ * cargo test --package rust-counter-tutorial -- --nocapture
+ * Note: 'rust-counter-tutorial' comes from cargo.toml's 'name' key
+ */
+
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+
+    // part of writing unit tests is setting up a mock context
+    // in this example, this is only needed for env::log in the contract
+    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+        VMContext {
+            current_account_id: "alice_near".to_string(),
+            signer_account_id: "robert_near".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "jane_near".to_string(),
+            input,
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view,
+            output_data_receivers: vec![],
+        }
+    }
+
+    // mark individual unit tests with #[test] for them to be registered and fired
+    #[test]
+    fn increment() {
+        // set up the mock context into the testing environment
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        // instantiate a contract variable with the counter at zero
+        let mut contract = Counter{ val: 0 };
+        contract.increment();
+        println!("Value after increment: {}", contract.get_num());
+        // confirm that we received 1 when calling get_num
+        assert_eq!(1, contract.get_num());
+    }
+
+    #[test]
+    fn decrement() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let mut contract = Counter{ val: 0 };
+        contract.decrement();
+        println!("Value after decrement: {}", contract.get_num());
+        // confirm that we received -1 when calling get_num
+        assert_eq!(-1, contract.get_num());
+    }
+
+    #[test]
+    fn increment_and_reset() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let mut contract = Counter{ val: 0 };
+        contract.increment();
+        contract.reset();
+        println!("Value after reset: {}", contract.get_num());
+        // confirm that we received -1 when calling get_num
+        assert_eq!(0, contract.get_num());
+    }
+}
