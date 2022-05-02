a7b7b23aa02d944655241c34a88bcf0443eeb5d2
Dennis <10233439+idea404@users.noreply.github.com>
2022-05-02 18:06:46 +0100

    basic structure for workspaces testing

---
+++ integration-tests/ts/basic.ava.ts
@@ -0,0 +1,71 @@
+/**
+ * This test demonstrates basic behavior of near-workspaces, making simple
+ * function calls and view calls to the contract from
+ * https://github.com/near-examples/rust-status-message
+ *
+ * Note that the same tests will be run on both a local sandbox environment and
+ * on testnet by using the `test:sandbox` and `test:testnet` scripts in
+ * package.json.
+ */
+import { Worker, NEAR, NearAccount } from "near-workspaces";
+import anyTest, { TestFn } from "ava";
+
+const test = anyTest as TestFn<{
+  worker: Worker;
+  accounts: Record<string, NearAccount>;
+}>;
+
+test.beforeEach(async (t) => {
+  // Init the worker and start a Sandbox server
+  const worker = await Worker.init();
+
+  // Prepare sandbox for tests, create accounts, deploy contracts, etx.
+  const root = worker.rootAccount;
+  const contract = await root.createAndDeploy(
+    root.getSubAccount("rust-counter").accountId,
+    "../../../out/main.wasm",
+    { initialBalance: NEAR.parse("3 N").toJSON() }
+  );
+  const ali = await root.createSubAccount("ali", {
+    initialBalance: NEAR.parse("3 N").toJSON(),
+  });
+
+  // Save state for test runs, it is unique for each test
+  t.context.worker = worker;
+  t.context.accounts = { root, contract, ali };
+});
+
+test.afterEach(async (t) => {
+  // Stop Sandbox server
+  await t.context.worker.tearDown().catch((error) => {
+    console.log("Failed to stop the Sandbox:", error);
+  });
+});
+
+test("Root gets null status", async (t) => {
+  const { root, contract } = t.context.accounts;
+  const result: null = await contract.view("get_status", {
+    account_id: root.accountId,
+  });
+  t.is(result, null);
+});
+
+test("Ali sets then gets status", async (t) => {
+  const { ali, contract } = t.context.accounts;
+  await ali.call(contract, "set_status", { message: "hello" });
+  const result: string = await contract.view("get_status", { account_id: ali });
+  t.is(result, "hello");
+});
+
+test("Root and Ali have different statuses", async (t) => {
+  const { root, contract, ali } = t.context.accounts;
+  await root.call(contract, "set_status", { message: "world" });
+  const rootStatus: string = await contract.view("get_status", {
+    account_id: root,
+  });
+  t.is(rootStatus, "world");
+  const aliStatus: null = await contract.view("get_status", {
+    account_id: ali,
+  });
+  t.is(aliStatus, null);
+});
